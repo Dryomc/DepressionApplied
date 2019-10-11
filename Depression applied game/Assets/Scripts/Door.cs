a6f77246dfd7a7551d3910b3bdbfed3fa2db0a69
Dryomc <31847803+Dryomc@users.noreply.github.com>
2019-10-11 14:53:10 +0200

    Entire Project

---
+++ Depression applied game/Assets/Scripts/Door.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _Doorstep;
+
+    private float _StartPosition;
+    private void Start()
+    {
+        _StartPosition = transform.position.y;
+    }
+
+    void Update()
+    {
+        float yPosition = _StartPosition + (_Doorstep.transform.localScale.y / 2);
+
+        transform.position = new Vector3(transform.position.x, yPosition, transform.position.z);
+    }
+}
---
+++ Depression applied game/Assets/Scripts/DoorStopStretcher.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorStopStretcher : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _DoorStep, _Player;
+
+    private float _Distance;
+
+    private float _Percentage;
+
+    private float originalyScale;
+
+    private void Start()
+    {
+        originalyScale = _DoorStep.transform.localScale.y;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            _Distance = Vector3.Distance(_DoorStep.transform.position, _Player.transform.position);
+            _Percentage = 100;
+            //Debug.Log("Entered");
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            float scaleMultiplier = 101 - _Percentage;
+            _DoorStep.transform.localScale = new Vector3(_DoorStep.transform.localScale.x, originalyScale * scaleMultiplier, _DoorStep.transform.localScale.z);
+            _Percentage = (Vector3.Distance(_DoorStep.transform.position, _Player.transform.position) / _Distance) * 100;
+            //Debug.Log(_Percentage);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _Percentage = 100;
+    }
+}
